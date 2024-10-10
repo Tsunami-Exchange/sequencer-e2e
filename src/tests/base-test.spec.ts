db90c59416b5f6ce1f7a413e9800aad690f1b8fe
ForsakenWing <Fors2288@gmail.com>
2024-10-10 16:36:34 +0400

    feat: adding stop market orderType

--- src/tests/base-test.spec.ts
+++ src/tests/base-test.spec.ts
@@ -2,56 +2,87 @@ import { ActivePositionsValidator, ActivePositionValidator } from '@/common/safe
 import { ActivePositionsQuery, ActivePositionsVariables } from '@/queries/ActivePositions';
 import { Config } from '@/utils/config';
 import { GRAPHQL_URL } from '@/utils/constants';
+import { _SdkManager } from '@/utils/sdk-manager';
 import { sendToSequencer } from '@/utils/sequencer';
 import { test } from '@fixtures/baseFixture';
-import { Direction } from '@storm-trade/sdk';
-import { beginCell, external, internal, storeMessage } from '@ton/core';
+import { AsyncCreateOrderParams, Direction, orderTypesNumsToNames, OrderTypes } from '@storm-trade/sdk';
+import { Address, beginCell, external, internal, storeMessage } from '@ton/core';
 import { request as gqlRequest } from 'graphql-request';
 import { expect } from 'playwright/test';
 import { z } from 'zod';
+import { Market } from '../utils/config';
 
 // We can't run in parallel because of the seqno can't transfer from the same wallet in parallel
-test('Verify that order can be created and handled by sequencer', async ({ wallet, sdkManager, config, db }) => {
-  const market = config.getMarket('BTC/USDT');
-  const { vaultAddress, baseAsset, quoteAssetId, address } = market;
-  const traderAddress = wallet.getTonAddress();
-  const quoteAssetName = Config.assetIdToName(quoteAssetId);
-  const ASSET_AMOUNT = 1;
-  const LEVERAGE = 50;
-  const stopLossTriggerPrice = 99.5;
-  const transaction = await sdkManager.createOrder(vaultAddress, {
-    orderType: 'market',
+
+await Config.init();
+const MARKET = Config.getMarket('BTC/USDT');
+const { vaultAddress, quoteAssetId, baseAsset } = MARKET;
+const quoteAssetName = Config.assetIdToName(quoteAssetId);
+
+const orderParams = [
+  {
+    orderType: 'market' as const,
     direction: Direction['long'],
+    leverage: BigInt(50 * 1e9),
+    amount: Config.toAsset(quoteAssetName, 1),
     baseAsset,
-    traderAddress,
-    amount: Config.toAsset(quoteAssetName, ASSET_AMOUNT),
-    leverage: BigInt(LEVERAGE * 1e9),
     expiration: Math.ceil((Date.now() + 24 * 60 * 60 * 1000) / 1000),
-    stopTriggerPrice: Config.toAsset('TON', stopLossTriggerPrice),
+    stopTriggerPrice: 0n,
     takeTriggerPrice: 0n,
     limitPrice: 0n,
+  },
+  // Default limit order
+  {
+    orderType: 'stopLimit' as const,
+    direction: Direction['short'],
+    leverage: BigInt(50 * 1e9),
+    amount: Config.toAsset(quoteAssetName, 1),
+    baseAsset,
+    expiration: Math.ceil((Date.now() + 24 * 60 * 60 * 1000) / 1000),
+    stopTriggerPrice: Config.toAsset(quoteAssetName, 99.5),
+    takeTriggerPrice: 0n,
+    stopPrice: 0n,
+    limitPrice: Config.toAsset(quoteAssetName, 100),
+  },
+  // stopLimit order type
+  {
+    orderType: 'stopLimit' as const,
+    direction: Direction['short'],
+    leverage: BigInt(50 * 1e9),
+    amount: Config.toAsset(quoteAssetName, 1),
+    baseAsset,
+    expiration: Math.ceil((Date.now() + 24 * 60 * 60 * 1000) / 1000),
+    stopTriggerPrice: Config.toAsset(quoteAssetName, 99.5),
+    takeTriggerPrice: 0n,
+    stopPrice: 100n,
+    limitPrice: Config.toAsset(quoteAssetName, 100),
+  },
+];
+
+// create separate test where market first would be opened and then separate transaction to create takeProfit position and StopLoss position in the same direction
+
+orderParams.forEach(async (params) => {
+  test(`Verify that order ${params.orderType} with ${params.stopPrice} can be created and handled by sequencer`, async ({
+    wallet,
+    sdkManager,
+    db,
+  }) => {
+    const traderAddress = wallet.getTonAddress();
+    const transaction = await sdkManager.createOrder(vaultAddress, { ...params, traderAddress });
+    const seqno = await wallet.getSeqno();
+    const transfer = await wallet.createTransfer([internal(transaction)], seqno);
+    const ext = beginCell()
+      .store(storeMessage(external({ body: transfer, to: wallet.getTonAddress() })))
+      .endCell();
+    await sendToSequencer(ext);
+    await wallet.waitSeqno(seqno);
+    const traderRawString = traderAddress.toRawString();
+    await new Promise((resolve) => setTimeout(resolve, 120 * 1000));
+    const promises = [await db.getOrderHistory(traderRawString), await db.getOrderV2(traderRawString), await db.getTraderPositions(traderRawString)];
+    const [orderHistory, ordersV2, traderPositions] = await Promise.all(promises);
+    console.log('trader address', traderRawString);
+    console.log('orders', orderHistory);
+    console.log('ordersV2', ordersV2);
+    console.log('traderPositions', traderPositions);
   });
-  const seqno = await wallet.getSeqno();
-  const transfer = await wallet.createTransfer([internal(transaction)], seqno);
-  const ext = beginCell()
-    .store(storeMessage(external({ body: transfer, to: wallet.getTonAddress() })))
-    .endCell();
-  await sendToSequencer(ext);
-  await wallet.waitSeqno(seqno);
-  const traderRawString = traderAddress.toRawString();
-  // Graphql checks are gonna be preset only for this test because the GOAL is to test database consistensy only
-  const gqlResponse: { getActivePositions: z.infer<typeof ActivePositionsValidator> } = await gqlRequest({
-    url: GRAPHQL_URL,
-    document: ActivePositionsQuery(),
-    variables: ActivePositionsVariables(traderRawString, address),
-  });
-  expect(ActivePositionValidator.safeParse(gqlResponse.getActivePositions.long).success).toBeTruthy();
-  expect(ActivePositionsValidator.safeParse(gqlResponse.getActivePositions).success).toBeTruthy();
-  expect(gqlResponse.getActivePositions.orders).toHaveLength(1);
-  const traderPositions = await db.getTraderPositions(traderRawString);
-  const orderHistory = await db.getOrderHistory(traderRawString);
-  const ordersV2 = await db.getOrderV2(traderRawString);
-  console.log('ordersV2', ordersV2);
-  console.log('traderPositions', traderPositions);
-  console.log('orderHistory', orderHistory);
 });
