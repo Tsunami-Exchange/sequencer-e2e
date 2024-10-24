18e1d2e9c332680206134c4a3095cfa619c8e00e
ForsakenWing <Fors2288@gmail.com>
2024-10-24 11:58:13 +0400

    fix: changing path to globalsetup 2nd time

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  globalSetup: path.resolve(new URL('.', import.meta.url).pathname.replace(/\\/g, '/'), './globalSetup.ts'),
+  globalSetup: path.resolve('./globalSetup.ts'),
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
